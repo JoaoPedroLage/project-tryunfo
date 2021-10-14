ec210129e847c42db1cfd054102890b03c3e668d
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-14 10:51:41 -0300

    Tries to pass in the first requirement

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,21 @@
 export default class Form extends React.Component {
   render() {
     return (
-      <form />
+      <form>
+        <text data-testid="name-input" />
+        <textarea data-testid="description-input" />
+        <number data-testid="attr1-input" />
+        <number data-testid="attr2-input" />
+        <number data-testid="attr3-input" />
+        <text data-testid="attr3-input" />
+        <select data-testid="rare-input">
+          <option>normal</option>
+          <option>raro</option>
+          <option>muito raro</option>
+        </select>
+        <checkbox data-testid="trunfo-input" />
+        <button data-testid="save-button" type="submit">Salvar</button>
+      </form>
     );
   }
 }
