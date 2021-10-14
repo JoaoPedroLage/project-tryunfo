83e4bbb2264d0127cd42b7e93ffdd9a2974a785c
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-14 15:23:01 -0300

    In the Form: add inputs and labels to them

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,12 +4,30 @@ export default class Form extends React.Component {
   render() {
     return (
       <form>
-        <text data-testid="name-input" />
-        <textarea data-testid="description-input" />
-        <number data-testid="attr1-input" />
-        <number data-testid="attr2-input" />
-        <number data-testid="attr3-input" />
-        <text data-testid="attr3-input" />
+        <label htmlFor="name-input">
+          Nome da carta:
+          <input type="text" data-testid="name-input" />
+        </label>
+        <label htmlFor="description-input">
+          Descrição da carta:
+          <input type="textarea" data-testid="description-input" />
+        </label>
+        <label htmlFor="attr1-input">
+          Inserir o primeiro atributo da carta
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label htmlFor="attr2-input">
+          Inserir o segundo atributo da carta
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label htmlFor="attr3-input">
+          Inserir o terceiro atributo da carta
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label htmlFor="image-input">
+          Inserir o caminho para imagem da carta
+          <input type="text" data-testid="image-input" />
+        </label>
         <select data-testid="rare-input">
           <option>normal</option>
           <option>raro</option>
